60ffa6e97ede5d711ad74595905d09323b4989eb
Mansi_Patel <patelmaya9812@gmail.com>
2025-11-26 00:01:47 +0530

    Update hero.tsx

--- components/hero.tsx
+++ components/hero.tsx
@@ -144,6 +144,11 @@ export default function Hero() {
             initial={{ opacity: 0, scale: 0.8 }}
             animate={{ opacity: 1, scale: 1 }}
             transition={{ delay: 0.6, duration: 0.6 }}
+            onClick={() => {
+              document.querySelector("#countdown")?.scrollIntoView({
+                behavior: "smooth",
+              });
+            }}
           >
             Pre-order Now
           </motion.button>
